00e927de4566210439f871ed8113497e6a0e282f
myvpn1 <137178860+myvpn1@users.noreply.github.com>
2024-01-07 15:50:54 +0700

    Update register.sh

--- register.sh
+++ register.sh
@@ -3,7 +3,7 @@
 #install
 apt update && apt upgrade
 apt install python3 python3-pip git
-git clone https://github.com/myvpn1/Register.git
+wget https://raw.githubusercontent.com/myvpn1/Register/main/regis.zip
 unzip regis.zip
 pip3 install -r regis/requirements.txt
 pip3 install pillow
